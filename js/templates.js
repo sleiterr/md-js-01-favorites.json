bf12ce28a3153b18bff7e8b98a20332d88226b01
Oleg <sleite4r@gmail.com>
2025-02-18 21:20:10 +0100

    style favorite-section, add icon delete

--- js/templates.js
+++ js/templates.js
@@ -27,15 +27,15 @@ export const productPageTmpl = (product) =>
 
 export const FavListTmpl = (product) =>
   `
-    <div class='products-card'>
+    <div class='fav-card'>
       <img src="${product.image}${product.id}">
-     <div class='content-items'>
-      <p class='card-title'>${product.title}</p>
-      <p class='price-item'><i>${product.price} kr</i></p>
+     <div class='fav-items'>
+      <p class='fav-title'>${product.title}</p>
+      <p class='price-fav'><i>${product.price} kr</i></p>
      </div>
-     <div class='card-bttn'>
-      <button class='product-bttn'><a href="page.html?id=${product.id}" class='card-link'>Se produkt</a></button>
-      <button class="delete-favBtn" id="${product.id}">Delete favorit</button> 
+     <div class='fav-bttn'>
+     <button class="delete-favBtn" id="${product.id}"><i class="fa-solid fa-trash-can delete"></i></button> 
+      <button class='view-bttn'><a href="page.html?id=${product.id}" class='view-link'>Se produkt</a></button>
      </div>
     </div>
   `;
