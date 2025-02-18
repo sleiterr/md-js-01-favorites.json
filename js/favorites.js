2d7b16b0e2dd3a1b42daa791f3a4e52ce8a2ace4
Oleg <sleite4r@gmail.com>
2025-02-18 11:49:52 +0100

    update favoritContainer

--- js/favorites.js
+++ js/favorites.js
@@ -6,37 +6,52 @@ const products = await fetchProducts();
 export const favorites = () => {
   const favoriteContainer = document.querySelector(".favorite-container");
   let favoritesArray = JSON.parse(localStorage.getItem("favList")) || [];
+  /* localStorage.setItem('favList', 'test') */
 
-  if (favoriteContainer) {
-    /* localStorage.setItem('favList', 'test') */
-
-    const addToFav = (e) => {
-      const productID = e.target.id;
-      const productToAdd = products.find((product) => product.id == productID);
-
-      const exist = favoritesArray.find((product) => product.id == productID);
-
-      if (!exist) {
-        favoritesArray.push(productToAdd);
-
-        localStorage.setItem("favList", JSON.stringify(favoritesArray));
-        // console.log(favoritesArray);
+  const favoriteList = () => {
+    if (favoriteContainer) {
+      if (favoritesArray.length != 0) {
+        favoriteContainer.innerHTML = "";
+        favoritesArray.forEach((fav) => {
+          favoriteContainer.insertAdjacentHTML("beforeend", FavListTmpl(fav));
+        });
       } else {
-        console.log("producte er allerede føjet til favorite");
+        favoriteContainer.insertAdjacentHTML(
+          "beforeend",
+          "Der er ikke tilføjet nogle favorite"
+        );
       }
-    };
+    }
+  };
+  favoriteList();
 
-    const favBtn = document.querySelectorAll(".favBtn");
-    favBtn.forEach((btn) => {
-      btn.addEventListener("click", addToFav);
-    });
+  const addToFav = (e) => {
+    const productID = e.target.id;
+    const productToAdd = products.find((product) => product.id == productID);
 
-    const favoriteList = () => {
-      favoritesArray.forEach((fav) => {
-        favoriteContainer.insertAdjacentHTML("afterend", FavListTmpl(fav));
-      });
-    };
+    const exist = favoritesArray.find((product) => product.id == productID);
 
+    if (!exist) {
+      favoritesArray.push(productToAdd);
+
+      localStorage.setItem("favList", JSON.stringify(favoritesArray));
+      // console.log(favoritesArray);
+    } else {
+      console.log("producte er allerede føjet til favorite");
+    }
     favoriteList();
-  }
+  };
+
+  const favBtn = document.querySelectorAll(".favBtn");
+  favBtn.forEach((btn) => {
+    btn.addEventListener("click", addToFav);
+  });
+
+  // const favoriteList = () => {
+  //   favoriteContainer.innerHTML = "";
+  //   favoritesArray.forEach((fav) => {
+  //     favoriteContainer.insertAdjacentHTML("afterend", FavListTmpl(fav));
+  //   });
+
+  // favoriteList();
 };
