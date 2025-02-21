1fa44dd6bf6807dbe979a281c6b365179423d5e8
Oleg <sleite4r@gmail.com>
2025-02-21 19:27:39 +0100

    style serch bar

--- js/templates.js
+++ js/templates.js
@@ -7,11 +7,11 @@ export const productListTmpl = (product) =>
      <div class='content-items'>
       <p class='card-title'>${product.title}</p>
       <p class='price-item'><i>${product.price} kr</i></p>
-     </div>
-     <div class='card-bttn'>
-      <button class='product-bttn'><a href="page.html?id=${product.id}" class='card-link'>Se produkt</a></button>
-      <button class="favBtn" id="${product.id}">Føj til favoritter</button> 
-     </div>
+      </div>
+      <div class='card-bttn'>
+       <button class='product-bttn'><a href="page.html?id=${product.id}" class='card-link'>Se produkt</a></button>
+       <button class="favBtn" id="${product.id}">Føj til favoritter</button> 
+      </div>
     </div>
   `;
 export const productPageTmpl = (product) =>
@@ -28,8 +28,8 @@ export const productPageTmpl = (product) =>
    </div>
    
    `;
-  //  <button class='view-btt'><a href="index.html" class='view-link'>Tilbage</a></button> 
-   
+//  <button class='view-btt'><a href="index.html" class='view-link'>Tilbage</a></button>
+
 export const FavListTmpl = (product) =>
   `
     <div class='fav-card'>
